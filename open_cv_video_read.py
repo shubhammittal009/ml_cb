c512afc7cd251b10f35c087c7776b768fa4da260
Shubham Mittal <shubhammittal5114@gmail.com>
2019-06-12 13:46:47 +0530

    using_opencv

--- open_cv_video_read.py
+++ open_cv_video_read.py
@@ -1,4 +1,5 @@
 import cv2
+import numpy as np
 
 cam = cv2.VideoCapture(0)
 
@@ -13,7 +14,15 @@ while True:
 	if key_pressed == ord('q'): #ord--> ASCII Values(8 bit)
 		break
 
-	cv2.imshow("Video Title",frame)
+	gray = cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY)
+
+	# print(frame.shape)
+	# print(gray.shape)
+
+	cv2.imshow("RGB Title",frame)
+	cv2.imshow("GRAYSCALE Title",gray)
+
+	cv2.imshow("RGB Title",frame)
 
 cam.release()
 cv2.destroyAllWindows()
